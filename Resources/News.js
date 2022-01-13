002721412da8b14731acad4568f35fe198adefb3
intykat <bruvecat@gmail.com>
2022-01-12 20:29:21 -0600

    WOOOO I DID IT AUTOMATIC DEPLOYMENTTTT

--- Resources/News.js
+++ Resources/News.js
@@ -111,44 +111,5 @@ News = {
       "title": "Words of Terror, new dev, and more!",
       "date": "December 14th, 2021",
       "id" : "wot-release"
-    },
-    "news676767": {
-      "author": "iadsasdntykat",
-      "authorLink": "https://black-catstudios.com/developers/?intykat",
-      "img" : "https://black-catstudios.com/Images/NewsImages/woticon.png",
-      "description": `
-      <h3>  WoT?!?!?!? </h3>
-      Hey everyone! Today I am proud to announce "Words of Terror". Words of Terror, aka WoT, is Black Cat Studuio's first ever plugin.
-      Developed by <a href="https://black-catstudios.com/developers/?intykat"> intykat </a> and <a href="https://black-catstudios.com/developers/?volking"> Volking </a>, WoT is a plugin designed to do the OPPOSITE of helping you.
-      <br>
-      You see WoT is a plugin that CONSTANTLY throws insults at you. You can see a full list of them <a href="https://black-catstudios.com/WoT-Util/Demotivation-Words.json"> here. </a>
-      Me and Volking had just randomly thought of it for fun and thought we should do it for learning how to make more plugins in the future. Yes, this means we will be working on some more :)
-      <br>
-      <a href="https://www.roblox.com/library/6856957291/Words-of-terror"> Install the plugin here </a>
-      <br> <br>
-      <h3> Whim has joined! </h3>
-      A new developer is here! His name is whim, and he is one of the most godly scripters I have ever seen in my entire life. Seriously.
-      He was working on another project and offered to help us out with bcs out of pure generosity.
-      whim may be helping us with some really cool project we have lined up for the future!
-      <br> <br>
-      <h3> Other updates! </h3>
-      The Discord is getting some new stickers & emoji for kitter! 
-      These include:
-      <ul>
-      <li> Expressionless </li>
-      <li> Scream </li>
-      <li> Blush </li>
-      <li> Raised Eyebrow </li>
-      </ul>
-      We hope you'll enjoy them.
-      <br>
-      Additionally, the annual BCS roblox shirt is avaliable again!
-      <a href="https://www.roblox.com/catalog/6048947949/Chirstmas-limted-BCS-shirt"> Check it out here </a>
-      <br> <br>Anyway, cya guys with more exciting news soon! Bya!
-
-      `,
-      "title": "Words of Terror, new dev, and more!",
-      "date": "December 14th, 2021",
-      "id" : "wot-resa"
     }
 }
